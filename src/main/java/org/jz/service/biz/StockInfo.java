ce77d5825e28ec80ab2b78e6bbe201d0026048f3
hongyi zheng <635072863@qq.com>
2018-07-11 17:48:34 +0800

    update

--- src/main/java/org/jz/service/biz/StockInfo.java
+++ src/main/java/org/jz/service/biz/StockInfo.java
@@ -1,5 +1,6 @@
 package org.jz.service.biz;
 
+import org.springframework.stereotype.Component;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
@@ -8,17 +9,19 @@ import us.codecraft.webmagic.processor.PageProcessor;
  * @author Hongyi Zheng
  * @date 2018/7/11
  */
+@Component("stockInfo")
 public class StockInfo implements PageProcessor {
 
-    private Site site = Site.me();
+    private Site site = Site.me().setSleepTime(1500).setRetryTimes(3);
 
     @Override
     public void process(Page page) {
         if ("".equals(page.getUrl().xpath("").toString())) {
             page.setSkip(true);
+        }else {
+            page.putField("",page.getUrl().regex(""));
+            page.putField("",page.getHtml().xpath("").toString());
         }
-        page.putField("",page.getUrl().regex(""));
-        page.putField("",page.getHtml().xpath("").toString());
 
 
     }
